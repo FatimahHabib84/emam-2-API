fdfb9057f7619775c9837d444a602a1943adc2b5
FatimahHabib84 <fatimahhabib84@gmail.com>
2024-06-05 20:50:23 +0300

    editt

--- api1.js
+++ api1.js
@@ -4,14 +4,14 @@ async function getData (){
     let res = await fetch(url)
     let json = await res.json()
     console.log(json);
-    p[0].innerText=p[0].innerText+" "+json.data[0].hijri.date
+    p[0].innerText=p[0].innerText+" "+json.data[0].date.hijri.date
     p[1].innerText=p[1].innerText+" "+json.data[0].date.gregorian.date
-    p[2].innerText=p[2].innerText+" "+json.data[0].hijri.weekday.en
+    p[2].innerText=p[2].innerText+" "+json.data[0].date.hijri.weekday.en
     p[3].innerText=p[3].innerText+" "+json.data[0].timings.Fajr
-    p[3].innerText=p[4].innerText+" "+json.data[0].timings.Dhuhr
-    p[3].innerText=p[5].innerText+" "+json.data[0].timings.Asr
-    p[3].innerText=p[6].innerText+" "+json.data[0].timings.Maghrib
-    p[3].innerText=p[7].innerText+" "+json.data[0].timings.Isha
+    p[4].innerText=p[4].innerText+" "+json.data[0].timings.Dhuhr
+    p[5].innerText=p[5].innerText+" "+json.data[0].timings.Asr
+    p[6].innerText=p[6].innerText+" "+json.data[0].timings.Maghrib
+    p[7].innerText=p[7].innerText+" "+json.data[0].timings.Isha
 
 
     // p[4].innerText=p[4].innerText+" "+json.coord.lat
